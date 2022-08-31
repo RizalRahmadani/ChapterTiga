bce703b3e6a836d47aee3f3c3170f0655c4ddefe
RizalRahmadani <rizalrahmadani98@gmail.com>
2022-08-31 16:13:48 +0700

    menambah activity_home.xml dan ActivityHome.kt serta merubah AndroidManifest.xmli

---
+++ app/src/main/java/com/example/chaptertiga/ActivityHome.kt
@@ -0,0 +1,24 @@
+package com.example.chaptertiga
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+
+class ActivityHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        btnKalkulator.setOnClickListener {
+            var pindah = Intent(this,AplikasiBmiActivity:: class.java)
+            startActivity(pindah)
+        }
+        btnNilaiAkhirMahasiswa.setOnClickListener {
+            var pindah = Intent (this, ActivityNilaiAkhirMahasiswa::class.java)
+            startActivity(pindah)
+        }
+        btnPenjumlahan.setOnClickListener {
+            var pindah = Intent(this ,PenjumlahanActivity::class.java)
+        }
+    }
+}
