5b406e78d042385ea07f60471b9f9b6f25b9ae32
RizalRahmadani <rizalrahmadani98@gmail.com>
2022-08-31 16:06:24 +0700

    Menambahkan tugas menghitung nilai akhir

---
+++ app/src/main/java/com/example/chaptertiga/ActivityNilaiAkhirMahasiswa.kt
@@ -0,0 +1,11 @@
+package com.example.chaptertiga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityNilaiAkhirMahasiswa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nilai_akhir_mahasiswa)
+    }
+}
